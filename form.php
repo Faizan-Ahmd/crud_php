8d5aa12e668976ded54d1ee434780dc660ccacfc
Faizan Ahmad <faizanahmad1033@gmail.com>
2023-05-18 10:25:31 +0500

    registration form

---
+++ form.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css"/> 
+        <title>Form</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="title">Registration Form</div>
+            <div class="form">
+            <div class="form_field">
+                <label>First Name</label>
+                <input type="text" class="input"/>
+                
+            </div>
+            <div class="form_field">
+                <label>Last Name</label>
+                <input type="text" class="input"/>
+                
+            </div>
+            <div class="form_field">
+                <label>Password</label>
+                <input type="password" class="input"/>
+                
+            </div>
+            <div class="form_field">
+                <label>Confirm Password</label>
+                <input type="password" class="input"/>
+                
+            </div>
+            <div class="form_field">
+                <label>Gender</label>
+               <select>
+                <option>Select</option>
+                <option>Female</option>
+                <option>Male</option>
+               </select>
+                
+            </div> 
+            <div class="form_field">
+                <label>Email</label>
+                <input type="text" class="input"/>
+                
+            </div>
+            <div class="form_field">
+                <label>Phone number</label>
+                <input type="text" class="input"/>
+                
+            </div>
+            <div class="form_field">
+                <label>Address</label>
+                <textarea></textarea>
+                
+            </div>    
+            <div class="form_field">
+               <label class="check">
+                <input type="checkbox"/>
+                <span class=checkmark"></span>
+                <p>Agree to terms and conditions</p>
+               </label>
+                
+            </div>
+            <div class="input_field">
+                <input type="submit" value="register" class="btn"/>
+            </div>
+            </div>   
+        </div>
+    </body>
+</HTML>
